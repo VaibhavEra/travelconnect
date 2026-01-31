e71e0c58e4b62f9df23f2c2973b9d045e1717efe
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2026-01-31 08:58:34 +0000

    refactor(components): export CategoryBadge and FilterChip
    Add CategoryBadge and FilterChip to shared component exports.

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,4 +1,6 @@
 export { default as BaseModal } from "./BaseModal";
+export * from "./CategoryBadge";
+export { default as FilterChip } from "./FilterChip";
 export { default as ModalButton } from "./ModalButton";
 export { default as ModeSwitcher } from "./ModeSwitcher";
 export { default as OfflineNotice } from "./OfflineNotice";
